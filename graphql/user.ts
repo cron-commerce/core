8bea3ac4b1801d973badfbbe091bfc5942d402ef
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-21 14:03:52 -0700

    graphql error for short passwords

--- graphql/user.ts
+++ graphql/user.ts
@@ -1,3 +1,4 @@
+import {UserInputError} from 'apollo-server-koa'
 import * as jwt from 'jsonwebtoken'
 import {getManager} from 'typeorm'
 
@@ -27,28 +28,33 @@ const createJWT = user => jwt.sign({sub: user.id}, process.env.JWT_SECRET)
 export const resolvers = {
   Mutation: {
     createUser: async (obj, args, context, info) => {
+      if (args.password.length < 6) { throw new UserInputError('Password too short') }
+
       const entityManager = getManager()
       const user = new User()
       user.password = args.password
       user.email = args.email
       await entityManager.save(user)
+
       user.token = createJWT(user)
       return user
     },
+
     loginUser: async (obj, args, context, info) => {
       const entityManager = getManager()
       const user = await entityManager.findOne(User, {where: {email: args.email}})
+
       if (user && user.isPasswordValid(args.password)) {
         user.token = createJWT(user)
         return user
       }
     },
   },
+
   Query: {
     user: async (obj, args, context, info) => {
       const entityManager = getManager()
-      const user = await entityManager.findOne(User, {where: {email: args.email}})
-      return user
+      return entityManager.findOne(User, {where: {email: args.email}})
     },
   },
 }
