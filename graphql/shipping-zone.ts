87d66e8798503d19df12c78b3e4c8304de978c80
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-22 07:29:33 -0700

    shipping zone graphql

--- graphql/shipping-zone.ts
+++ graphql/shipping-zone.ts
@@ -3,8 +3,62 @@ import * as Shopify from 'shopify-api-node'
 import {Shop} from '../entities/shop'
 
 const typeDef = `
+  type ShippingZoneWeightBasedShippingRate {
+    id: ID!
+    weight_low: Float
+    weight_high: Float
+    name: String
+    price: Float
+    shipping_zone_id: ID
+  }
+
+  type ShippingZonePriceBasedShippingRate {
+    id: ID!
+    name: String
+    min_order_subtotal: Float
+    price: Float
+    max_order_subtotal: Float
+    shipping_zone_id: ID
+  }
+
+  type ShippingZoneCarrierRateProvider {
+    id: ID!
+    country_id: ID
+    carrier_service_id: ID
+    percent_modifier: Int
+    flat_modifier: Float
+    shipping_zone_id: ID
+  }
+
+  type ShippingZoneProvince {
+    code: String
+    country_id: ID
+    id: ID!
+    name: String
+    shipping_zone_id: ID
+    tax: Float
+    tax_name: String
+    tax_type: String
+    tax_percentage: Float
+  }
+
+  type ShippingZoneCountry {
+    id: ID!
+    shipping_zone_id: ID
+    name: String
+    tax: Float
+    code: String
+    tax_name: String
+    provinces: [ShippingZoneProvince]
+  }
+
   type ShippingZone {
     id: ID!
+    name: String
+    countries: [ShippingZoneCountry]
+    weight_based_shipping_rates: [ShippingZoneWeightBasedShippingRate]
+    price_based_shipping_rates: [ShippingZonePriceBasedShippingRate]
+    carrier_shipping_rate_providers: [ShippingZoneCarrierRateProvider]
   }
 
   extend type Query {
